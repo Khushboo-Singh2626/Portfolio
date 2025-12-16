2b3c4390a5c16ed64aec975b82bd9c80cd837ccb
Khushboo-Singh2626 <khushboosingh9369@gmail.com>
2025-12-16 18:19:18 +0530

    fix error for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/Hero/Hero"
 import About from './components/about/About';
-import Services from './components/services/services';
+import Services from './components/services/Services';
 import Work from './components/work/Work';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
